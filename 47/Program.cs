7027a979b7958cfee708b3f5141d57cb31071232
John Doe <shvedok1@mail.ru>
2022-12-17 15:04:33 +0300

    всё

---
+++ 47/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте двумерный массив (вручную) размером m×n, заполненный случайными вещественными числами.
+Console.WriteLine("Ведите числа m и n");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+double [,] array2d = new double[m, n];
+Random random = new Random();
+for (int i=0; i<m;i++)
+{
+    for (int j=0; j<n; j++)
+    {
+        array2d[i,j]=random.NextDouble ()*9;
+        Console.Write(array2d[i,j]+" ");
+    }
+Console.WriteLine();
+}
---
+++ 52/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте двумерный массив (вручную) из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.Clear();
+Console.WriteLine("Ведите числа m и n");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] array2d = new int[m, n];
+decimal [] averegest = new decimal [n]; 
+Console.WriteLine("Введите элементы массива в одну строку через запятую");
+for (int i=0; i<m;i++)
+{
+    string text = Console.ReadLine();
+    string[] Array = text.Split(",");
+    for (int j=0; j<n; j++)
+    {
+        array2d[i,j]=Convert.ToInt32(Array[j]);
+        Console.Write(array2d[i,j]+" ");
+        averegest[j]=averegest[j]+(Convert.ToDecimal(array2d[i,j])/Convert.ToDecimal(m));
+    }
+Console.WriteLine();
+}
+Console.WriteLine("Средние арифмитические каждого столбца: ");
+Console.WriteLine(string.Join("; ",averegest));
